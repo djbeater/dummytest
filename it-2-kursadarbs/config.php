401c9567bebcc7b5efd45dbc59f2a707236b4c54
djbeater <raitis.rolis@gmail.com>
2014-04-16 00:20:40 +0300

    debug true

--- it-2-kursadarbs/config.php
+++ it-2-kursadarbs/config.php
@@ -3,5 +3,5 @@ define('DB_NAME', 'maris_test');
 define('DB_USER', 'maris');
 define('DB_PASS', '4cuxjFz2CFKqFQ82');
 define('DB_HOST', 'lvur.no-ip.biz');
-define('DEBUG', false);
+define('DEBUG', true);
 ?>
